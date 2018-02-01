a9e0ffe9c10574d190162020623ccd3b139df438
JamesYu <Adomikao@foxmail.com>
2018-02-01 16:50:06 +0800

    Update MMongodb.class.php

--- MMongodb.class.php
+++ MMongodb.class.php
@@ -223,3 +223,192 @@ class MMongodb {
             return false;
         }
     }
+    /**
+     * 查询表的记录数
+     *
+     * 参数：
+     * $table_name:表名
+     * $condition:查询条件
+     *
+     * 返回值：表的记录数
+     */
+    function count($table_name, $condition=array(), $options=array())
+    {
+        $dbname = $this->curr_db_name;   
+        $options['maxTimeMS'] = 60000;     
+        return $this->mongo->$dbname->$table_name->count($condition, $options);
+    }
+
+    /**
+     * 更新记录
+     *
+     * 参数：
+     * $table_name:表名
+     * $condition:更新条件
+     * $newdata:新的数据记录  $inc $set $unset $push $pushAll $addToSet $pop $pull $pullAll
+     * $options:更新选择-upsert/multiple
+     *
+     * 返回值：
+     * 成功：true
+     * 失败：false
+     */
+    function update($table_name, $condition, $newdata, $options=array())
+    {
+        $dbname = $this->curr_db_name;
+        $options['safe'] = 1;
+        if (!isset($options['multiple']))
+        {
+            $options['multiple'] = 0;
+        }
+        try {
+            $this->mongo->$dbname->$table_name->update($condition, $newdata, $options);
+            return true;
+        }
+        catch (MongoCursorException $e)
+        {
+            $this->error = $e->getMessage();
+            return false;
+        }
+    }
+
+    /**
+     * 修改更新记录
+     *
+     */
+    function updateSet($table_name, $condition, $newdata, $options=array())
+    {
+        return $this->update($table_name, $condition, array('$set'=>$newdata), $options);
+    }
+
+    /**
+     * 删除记录
+     *
+     * 参数：
+     * $table_name:表名
+     * $condition:删除条件
+     * $options:删除选择-justOne
+     *
+     * 返回值：
+     * 成功：true
+     * 失败：false
+     */
+    function delete($table_name, $condition, $options=array())
+    {
+        $dbname = $this->curr_db_name;
+        $options['safe'] = 1;
+        try {
+            $this->mongo->$dbname->$table_name->remove($condition, $options);
+            return true;
+        }
+        catch (MongoCursorException $e)
+        {
+            $this->error = $e->getMessage();
+            return false;
+        }
+    }
+
+    /**
+     * 查找记录
+     *
+     * 参数：
+     * $table_name:表名
+     * $query_condition:字段查找条件
+     * $result_condition:查询结果限制条件-limit/sort等
+     * $fields:获取字段
+     *
+     * 返回值：
+     * 成功：记录集
+     * 失败：false
+     */
+    function find($table_name, $query_condition, $result_condition=array(), $fields=array(), $iscursor=0)
+    {
+        $dbname = $this->curr_db_name;
+        $cursor = $this->mongo->$dbname->$table_name->find($query_condition, $fields);
+        $cursor->timeout(60000);
+        
+        if (!empty($result_condition['start']))
+        {
+            $cursor->skip($result_condition['start']);
+        }
+        if (!empty($result_condition['limit']))
+        {
+            $cursor->limit($result_condition['limit']);
+        }
+        if (!empty($result_condition['sort']))
+        {
+            $cursor->sort($result_condition['sort']);
+        }
+        if (!empty($result_condition['skip']))
+        {
+            $cursor->skip($result_condition['skip']);
+        }
+        if(!$iscursor)
+        {
+            $result = array();
+            try {
+                while ($cursor->hasNext())
+                {
+                    $result[] = $cursor->getNext();
+                }
+            }
+            catch (MongoConnectionException $e)
+            {
+                $this->error = $e->getMessage();
+                return false;
+            }
+            catch (MongoCursorTimeoutException $e)
+            {
+                $this->error = $e->getMessage();
+                return false;
+            }
+            return $result;
+        }
+        else
+        {
+            return $cursor;
+        }
+    }
+
+    /**
+     * 查找一条记录
+     *
+     * 参数：
+     * $table_name:表名
+     * $condition:查找条件
+     * $fields:获取字段
+     *
+     * 返回值：
+     * 成功：一条记录
+     * 失败：false
+     */
+    function findOne($table_name, $condition, $fields=array())
+    {
+        $dbname = $this->curr_db_name;
+        return $this->mongo->$dbname->$table_name->findOne($condition, $fields);
+    }
+
+    /**
+     * 获取当前错误信息
+     *
+     * 参数：无
+     *
+     * 返回值：当前错误信息
+     */
+    function getError()
+    {
+        return $this->error;
+    }
+
+    /**
+     * 获取数据库集合
+     *
+     * 参数：无
+     *
+     * 返回值：集合名称
+     */
+    function getCollection()
+    {
+        $dbname = $this->curr_db_name;
+        return $this->mongo->$dbname->listCollections();
+    }
+}
